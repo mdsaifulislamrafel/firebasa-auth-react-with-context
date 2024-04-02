fab814d3af71dbe53469cee8b1349340ab6436b3
Md Saiful Islam Rafel <mdsaifulisalmrafel@gmail.com>
2024-04-02 18:10:51 +0600

    login page adnd register page design complete

--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,7 +1,50 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    };
     return (
-        <div>
-            <h1>This is a register</h1>            
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Your name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                        <div className="form-control mt-6">
+                            <p>Already register please <Link to={"/login"} className="link link-hover">Login</Link></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
